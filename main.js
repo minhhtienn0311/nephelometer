4ca52ba2e9501bbe98e7b4885f9de4217111a86d
minhhtienn0311 <tienminle0311@gmail.com>
2024-04-18 21:41:06 +0700

    123

--- main.js
+++ main.js
@@ -25,6 +25,29 @@ navLinks.addEventListener("click", (e) => {
   menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
 });
 
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btn__user');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+  wrapper.classList.remove('active-popup');
+});
+
 const scrollRevealOption = {
   distance: "50px",
   origin: "bottom",
